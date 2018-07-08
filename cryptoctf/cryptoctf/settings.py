a2c1c691d7b4abc54ed8626c0e1ff0f76cc2a683
1phan <g40.chuan@gmail.com>
2018-07-08 16:03:28 +0800

    close debug

--- cryptoctf/cryptoctf/settings.py
+++ cryptoctf/cryptoctf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ti_yd#txhu&+$0#cju36ue+0g+g9ge$v2#vvftig-r7@j4t*95'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cryptoctf.club',  '172.245.170.110', '127.0.0.1']
 
